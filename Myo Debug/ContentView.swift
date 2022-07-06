c4fe7cd175ec104e90dad27a0ba8bc4e47056921
Shivam Sharma <shivam.sharm@icloud.com>
2022-07-06 15:05:56 -0400

    Initial Commit

---
+++ Myo Debug/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Myo Debug
+//
+//  Created by Shivam Sharma on 2022-07-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Myo Debug/Myo_DebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myo_DebugApp.swift
+//  Myo Debug
+//
+//  Created by Shivam Sharma on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct Myo_DebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
